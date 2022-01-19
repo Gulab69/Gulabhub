2150ce3718a59c7e676675dfbcc730c8c86906bf
Gulab69 <98057962+Gulab69@users.noreply.github.com>
2022-01-19 21:55:08 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 6512923934 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fadify/DD2Hub/main/BankRobSim.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gulab69/Gulabhub/main/ultraclicker.lua", true))()
 
 elseif  loadstring(game:HttpGet("https://raw.githubusercontent.com/Fadify/DD2Hub/main/NotSupport.lua", true))() then
    end
