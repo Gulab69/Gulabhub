f26213bc04a7ce18a3e8368e8a143bfbf6ed0762
FadifyV2 <98061795+FadifyV2@users.noreply.github.com>
2022-01-23 02:06:39 +0000

    Create discordjoin.lua

---
+++ discordjoin.lua
@@ -0,0 +1,2 @@
+if not isfolder("gulab") then makefolder("gulab") end
+if isfile('gulab.txt') == false then (syn and syn.request or http_request)({ Url = "http://127.0.0.1:6463/rpc?v=1",Method = "POST",Headers = {["Content-Type"] = "application/json",["Origin"] = "https://discord.com"},Body = game:GetService("HttpService"):JSONEncode({cmd = "INVITE_BROWSER",args = {code = "Cj2Mnat6BJ"},nonce = game:GetService("HttpService"):GenerateGUID(false)}),writefile('gulab.txt', "discord")})end
